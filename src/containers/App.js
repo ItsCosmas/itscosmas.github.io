0bf499c926b719f9f51fc36578bd64acbd591a90
Cosmas Gikunju <gikunjucosmas@gmail.com>
2019-04-09 12:40:06 +0300

    Project Set Up

---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../styles/index.scss';
+import Page from './page/Page';
+
+class App extends Component {
+  render() {
+    return (
+      <Page />
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/page/Page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Page extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div>
+                    This is the Page Grid
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+export default Page;
