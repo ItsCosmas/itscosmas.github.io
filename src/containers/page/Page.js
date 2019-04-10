39e5730e632b464746abc7df4fc7f9226bc55aac
Cosmas Gikunju <gikunjucosmas@gmail.com>
2019-04-10 17:47:17 +0300

    Added the Header Component

--- src/containers/page/Page.js
+++ src/containers/page/Page.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from '../../components/header/header';
 
 class Page extends React.Component {
     render() {
         return (
             <React.Fragment>
+                <Header />
                 <div>
                     This is the Page Grid
                 </div>
