d59d14570bc359c9f0155407b9cdd873975973f4
Cosmas Gikunju <gikunjucosmas@gmail.com>
2019-04-13 18:11:01 +0300

    Styling the social buttons

--- src/components/socialButtons/socialButtons.js
+++ src/components/socialButtons/socialButtons.js
@@ -5,26 +5,28 @@ class SocialButtons extends React.Component {
 
         return (
             <React.Fragment>
-                <a href={`https://linkedin.com/in/cosmasgikunju/`} className={`action`} target={`_blank`}>
-                    <i className="fab fa-linkedin-in"></i>
-                    <span>LinkedIn</span>
-                </a>
-                <a href={`https://github.com/ItsCosmas`} className={`action`} target={`_blank`}>
-                    <i className="fab fa-github"></i>
-                    <span>Github</span>
-                </a>
-                <a href={`https://medium.com/@gikunjucosmas`} className={`action`} target={`_blank`}>
-                    <i className="fab fa-medium-m"></i>
-                    <span>Blog</span>
-                </a>
-                <a href={`#`} className={`action`} >
-                    <i className="fas fa-at"></i>
-                    <span>Email</span>
-                </a>
-                <a href={`#`} className={`action`} target={`_blank`}>
-                    <i className="fas fa-newspaper"></i>
-                    <span>Resume</span>
-                </a>
+                <div className={'social__container'}>
+                    <a href={`https://linkedin.com/in/cosmasgikunju/`} className={`action`} target={`_blank`}>
+                        <i className="fab fa-linkedin-in"></i>
+                        <span>LinkedIn</span>
+                    </a>
+                    <a href={`https://github.com/ItsCosmas`} className={`action`} target={`_blank`}>
+                        <i className="fab fa-github"></i>
+                        <span>Github</span>
+                    </a>
+                    <a href={`https://medium.com/@gikunjucosmas`} className={`action`} target={`_blank`}>
+                        <i className="fab fa-medium-m"></i>
+                        <span>Blog</span>
+                    </a>
+                    <a href={`#`} className={`action`} >
+                        <i className="fas fa-at"></i>
+                        <span>Email</span>
+                    </a>
+                    <a href={`#`} className={`action`} target={`_blank`}>
+                        <i className="fas fa-newspaper"></i>
+                        <span>Resume</span>
+                    </a>
+                </div>
             </React.Fragment>
         );
 
