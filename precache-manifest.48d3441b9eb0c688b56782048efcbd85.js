3064fcab04a851847fbbc3822dac87da55e150ef
Cosmas Gikunju <gikunjucosmas@gmail.com>
2019-04-13 15:49:54 +0300

    Updates

---
+++ precache-manifest.48d3441b9eb0c688b56782048efcbd85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f46e91811801b27a4171",
+    "url": "/static/js/main.868e3b4f.chunk.js"
+  },
+  {
+    "revision": "e2773036b649ebe8c813",
+    "url": "/static/js/2.109a7dd4.chunk.js"
+  },
+  {
+    "revision": "f46e91811801b27a4171",
+    "url": "/static/css/main.7344a21e.chunk.css"
+  },
+  {
+    "revision": "a36be816028ce4f021a85ff974d124f5",
+    "url": "/index.html"
+  }
+];
