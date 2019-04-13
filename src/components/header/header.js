295a3d9cd5f40b39d32e5f77efd28e69c4f24505
Cosmas Gikunju <gikunjucosmas@gmail.com>
2019-04-13 22:31:52 +0300

    Separated the Navbar as a separate component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,26 +5,12 @@ import Social from '../socialButtons/socialButtons';
 const header = (props) => {
     return (
         <React.Fragment>
-            <div className="navbar">
-                <div className="navbar__logo-name">Cozy</div>
-
-                <nav className="navbar__items">
-
-                    <div>Skills</div>
-                    <div>About</div>
-                    <div>Projects</div>
-                    <div>Contact</div>
-
-                </nav>
-            </div>
-
             <div className="header">
                 <div className="header__intro-box">
                     <Typed />
-                    <Social />
 
-                    <div className="header__actions">
-                        {/* <ContactButtons /> */}
+                    <div className="header__social">
+                        <Social />
                     </div>
                 </div>
             </div>
--- src/containers/page/Page.js
+++ src/containers/page/Page.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Navbar from '../../components/navbar/navbar';
 import Header from '../../components/header/header';
 
 class Page extends React.Component {
     render() {
         return (
             <React.Fragment>
+                <Navbar />
                 <Header />
             </React.Fragment>
         )
