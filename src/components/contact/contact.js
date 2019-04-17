a54bd5e9ca9b839a5cd3359d6c669d89ab5cb04e
Cosmas Gikunju <gikunjucosmas@gmail.com>
2019-04-17 11:37:18 +0300

    Contact Page Compnent

---
+++ src/components/contact/contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const contact = (props) => {
+    return (
+        <div className="section__contact">
+            Contact Page Goes Here
+        </div>
+    )
+};
+export default contact;
