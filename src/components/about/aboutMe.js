447ae104b54d496d4f41ae2734d0d9d6405d907e
Cosmas Gikunju <gikunjucosmas@gmail.com>
2019-04-17 11:37:03 +0300

    About Me Page Component

---
+++ src/components/about/aboutMe.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const about = (props) => {
+    return (
+        <div className="section__technologies">
+            About Page Section Goes Here
+        </div>
+    )
+};
+export default about;
