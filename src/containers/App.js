897c35c138c744d491c8516275811015e57606d0
Cosmas Gikunju <gikunjucosmas@gmail.com>
2019-04-17 11:38:08 +0300

    Update technologies route to about Me route

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import Header from '../components/header/header';
+import Projects from '../components/projects/projects';
+import About from '../components/about/aboutMe';
+import Contact from '../components/contact/contact';
 import '../styles/index.scss';
 
 class App extends React.Component {
@@ -15,7 +18,7 @@ class App extends React.Component {
 
                             <ul className="navbar__items-links">
                                 <li><Link to="/" className="navbar__items-link">Home</Link></li>
-                                <li><Link to="/stack" className="navbar__items-link">Technologies</Link></li>
+                                <li><Link to="/about" className="navbar__items-link">About Me</Link></li>
                                 <li><Link to="/projects" className="navbar__items-link">Projects</Link></li>
                                 <li><Link to="/contact" className="navbar__items-link">Contact</Link></li>
                             </ul>
@@ -23,7 +26,7 @@ class App extends React.Component {
                         </nav>
                     </div>
                     <Route exact path="/" component={Header} />
-                    <Route path="/stack" component={Technologies} />
+                    <Route path="/about" component={About} />
                     <Route path="/projects" component={Projects} />
                     <Route path="/contact" component={Contact} />
                 </React.Fragment>
@@ -32,8 +35,5 @@ class App extends React.Component {
         )
     }
 }
-const Technologies = () => <div><h2>My Tech Stack</h2></div>
-const Projects = () => <div><h2>My Projects</h2></div>
-const Contact = () => <div><h2>My Contact Form</h2></div>
 
 export default App;
