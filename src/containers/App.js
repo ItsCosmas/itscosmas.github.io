dac070fc8019196be9a4742c2c0449cc427c7b96
Cosmas Gikunju <gikunjucosmas@gmail.com>
2019-04-21 16:47:45 +0300

    Edited the Router

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Header from '../components/header/header';
 import Projects from '../components/projects/projects';
 import About from '../components/about/aboutMe';
-import Contact from '../components/contact/contact';
 import '../styles/index.scss';
 
 class App extends React.Component {
     render() {
         return (
-            <BrowserRouter basename='/'>
+            <Router basename='/'>
                 <React.Fragment>
                     <div className="navbar">
                         <div className="navbar__logo-name">Cozy</div>
@@ -18,9 +17,8 @@ class App extends React.Component {
 
                             <ul className="navbar__items-links">
                                 <li><Link to="/" className="navbar__items-link">Home</Link></li>
-                                <li><Link to="/about" className="navbar__items-link">About Me</Link></li>
+                                <li><Link to="/about" className="navbar__items-link">About</Link></li>
                                 <li><Link to="/projects" className="navbar__items-link">Projects</Link></li>
-                                <li><Link to="/contact" className="navbar__items-link">Contact</Link></li>
                             </ul>
 
                         </nav>
@@ -28,9 +26,8 @@ class App extends React.Component {
                     <Route exact path="/" component={Header} />
                     <Route path="/about" component={About} />
                     <Route path="/projects" component={Projects} />
-                    <Route path="/contact" component={Contact} />
                 </React.Fragment>
-            </BrowserRouter>
+            </Router>
 
         )
     }
