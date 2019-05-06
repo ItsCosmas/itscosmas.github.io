91c96d95ac0507705a185de90f75f9aeb54da319
Cosmas Gikunju <gikunjucosmas@gmail.com>
2019-05-06 05:39:54 +0300

    Social Buttons should be a Stateless Component

--- src/components/socialButtons/socialButtons.js
+++ src/components/socialButtons/socialButtons.js
@@ -1,35 +1,31 @@
 import React from 'react';
 
-class SocialButtons extends React.Component {
-    render() {
-
-        return (
-            <React.Fragment>
-                <div className={'icons__social'}>
-                    <a href={`https://linkedin.com/in/cosmasgikunju/`} className={`icons__social-item`} target={`_blank`}>
-                        <i className="fab fa-linkedin-in"></i>
-                        <span>LinkedIn</span>
-                    </a>
-                    <a href={`https://github.com/ItsCosmas`} className={`icons__social-item`} target={`_blank`}>
-                        <i className="fab fa-github"></i>
-                        <span>Github</span>
-                    </a>
-                    <a href={`https://medium.com/@gikunjucosmas`} className={`icons__social-item`} target={`_blank`}>
-                        <i className="fab fa-medium-m"></i>
-                        <span>Blog</span>
-                    </a>
-                    <a href={`mailto:gikunjucosmas@gmail.com`} className={`icons__social-item`} >
-                        <i className="fas fa-at"></i>
-                        <span>Email</span>
-                    </a>
-                    <a href={`https://drive.google.com/file/d/1BU4iuL-txtj4GdldykoBkFBiAoBt_XuJ/view?usp=sharing`} className={`icons__social-item`} target={`_blank`}>
-                        <i className="fas fa-newspaper"></i>
-                        <span>Resume</span>
-                    </a>
-                </div>
-            </React.Fragment>
-        );
-
-    }
+const SocialButtons = (props) => {
+    return (
+        <React.Fragment>
+            <div className={'icons__social'}>
+                <a href={`https://linkedin.com/in/cosmasgikunju/`} className={`icons__social-item`} target={`_blank`}>
+                    <i className="fab fa-linkedin-in"></i>
+                    <span>LinkedIn</span>
+                </a>
+                <a href={`https://github.com/ItsCosmas`} className={`icons__social-item`} target={`_blank`}>
+                    <i className="fab fa-github"></i>
+                    <span>Github</span>
+                </a>
+                <a href={`https://medium.com/@gikunjucosmas`} className={`icons__social-item`} target={`_blank`}>
+                    <i className="fab fa-medium-m"></i>
+                    <span>Blog</span>
+                </a>
+                <a href={`mailto:gikunjucosmas@gmail.com`} className={`icons__social-item`} >
+                    <i className="fas fa-at"></i>
+                    <span>Email</span>
+                </a>
+                <a href={`https://drive.google.com/file/d/1BU4iuL-txtj4GdldykoBkFBiAoBt_XuJ/view?usp=sharing`} className={`icons__social-item`} target={`_blank`}>
+                    <i className="fas fa-newspaper"></i>
+                    <span>Resume</span>
+                </a>
+            </div>
+        </React.Fragment>
+    );
 }
 export default SocialButtons;
